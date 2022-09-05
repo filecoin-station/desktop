74a45bfbe76fa61837ae9471ed0df31110ac242e
Julian Gruber <julian@juliangruber.com>
2022-09-05 14:29:22 +0200

    Add expose module logs. Closes #94

--- main/app-menu.js
+++ main/app-menu.js
@@ -1,26 +1,42 @@
-const { Menu, MenuItem, ipcMain } = require('electron')
+const fs = require('node:fs/promises')
+const { Menu, MenuItem, ipcMain, dialog, BrowserWindow } = require('electron')
 const { ipcMainEvents } = require('./ipc')
+const { getLog } = require('./saturn-node')
 
 function setupAppMenu (/** @type {import('./typings').Context} */ ctx) {
-  // Add "Check for updates..." item to the Application menu on MacOS
-  // GitHub issues tracking the work to add this menu item on other platforms:
-  //  - Windows: https://github.com/filecoin-project/filecoin-station/issues/63
-  //  - Linux: https://github.com/filecoin-project/filecoin-station/issues/64
-  if (process.platform !== 'darwin') return
-
   const menu = Menu.getApplicationMenu()
   if (!menu) return
 
-  menu.items[0].submenu?.insert(1, new MenuItem({
-    id: 'checkForUpdates',
-    label: 'Check For Updates...',
-    click: () => { ctx.manualCheckForUpdates() }
-  }))
-  menu.items[0].submenu?.insert(2, new MenuItem({
-    id: 'checkingForUpdates',
-    label: 'Checking For Updates',
-    enabled: false,
-    visible: false
+  if (process.platform === 'darwin') {
+    // Add "Check for updates..." item to the Application menu on MacOS
+    // GitHub issues tracking the work to add this menu item on other platforms:
+    //  - Windows: https://github.com/filecoin-project/filecoin-station/issues/63
+    //  - Linux: https://github.com/filecoin-project/filecoin-station/issues/64
+    menu.items[0].submenu?.insert(1, new MenuItem({
+      id: 'checkForUpdates',
+      label: 'Check For Updates...',
+      click: () => { ctx.manualCheckForUpdates() }
+    }))
+    menu.items[0].submenu?.insert(2, new MenuItem({
+      id: 'checkingForUpdates',
+      label: 'Checking For Updates',
+      enabled: false,
+      visible: false
+    }))
+  }
+
+  menu.items[1].submenu?.insert(1, new MenuItem({
+    label: 'Save Module Logs As…',
+    click: async () => {
+      const opts = { defaultPath: 'station.txt' }
+      const win = BrowserWindow.getFocusedWindow()
+      const { filePath } = win
+        ? await dialog.showSaveDialog(win, opts)
+        : await dialog.showSaveDialog(opts)
+      if (filePath) {
+        await fs.writeFile(filePath, getLog())
+      }
+    }
   }))
 
   Menu.setApplicationMenu(menu)
