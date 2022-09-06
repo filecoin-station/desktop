7c61558986a64e750e1972a3e0965e0fdb05c315
Miroslav Bajtoš <saturn@bajtos.net>
2022-09-06 14:34:03 +0200

    feat: add unit-testing framework for backend
    - Write down this architecture decision
    - Setup Mocha + `node:assert`
    
    Signed-off-by: Miroslav Bajtoš <saturn@bajtos.net>

---
+++ .mocharc.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+  spec: [
+    'main/test/**/*.test.js'
+  ],
+}
+
---
+++ main/test/smoke.test.js
@@ -0,0 +1,7 @@
+const assert = require('node:assert')
+
+describe('smoke test', function () {
+  it('works', function () {
+    assert.strictEqual(true, true)
+  })
+})
