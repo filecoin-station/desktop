2adab4a4d16ae788dc849ab5bc4ecf9f7c95680c
Pedro Oliveira <poliveira@subvisual.co>
2022-09-08 15:47:20 +0100

    add datejs dep

--- renderer/src/components/ActivityLog.tsx
+++ renderer/src/components/ActivityLog.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { ActivityEventMessage } from '../typings'
+import * as dayjs from 'dayjs'
 
 interface ILogElement {
   time: EpochTimeStamp,
@@ -30,12 +31,10 @@ interface IDateSeparator {
 }
 
 const DateSeparator: FC<IDateSeparator> = ({ date }) => {
-  const today = new Intl.DateTimeFormat(window.navigator.language, {}).format(new Date())
-  const yesterday_ = new Date()
-  yesterday_.setDate(yesterday_.getDate() - 1)
-  const yesterday = new Intl.DateTimeFormat(window.navigator.language, {}).format(yesterday_)
-
-  const displayStr = date === today ? 'today' : date === yesterday ? 'yesterday' : date
+  const today = dayjs().format('YYYY-MM-DD')
+  const yesterday = dayjs().subtract(1, 'day').format('YYYY-MM-DD')
+  const displayStr = date === today ? 'today' : date === yesterday ? 'yesterday' : dayjs(date).format("D MMM")
+  
   return (
     <>
       <h1>{displayStr}</h1>
@@ -47,41 +46,35 @@ interface IActivityLog {
   logStream: ActivityEventMessage[] | undefined;
 }
 
-const ActivityLog: FC<IActivityLog> = ({ logStream }) => {
-  const sortedLogStream = logStream?.sort(function (x, y) {
+const ActivityLog: FC<IActivityLog> = ({ logStream=[] }) => {
+  const sortedLogStream = logStream.sort(function (x, y) {
     return x.time - y.time
   })
 
-  const groups = sortedLogStream?.reverse().reduce((groups: any, log: ActivityEventMessage) => {
-    const date = new Intl.DateTimeFormat(window.navigator.language, {}).format(new Date(log.time))
-
-    if (!groups[date]) {
-      groups[date] = []
-    }
-
-    groups[date] = [...groups[date], log]
-    return groups
+  const groups = sortedLogStream.reverse().reduce((groups: any, log: ActivityEventMessage) => {
+    const date = dayjs.unix(log.time/1000).format("YYYY-MM-DD")
+    return {...groups, [date]: groups[date] ? groups[date].concat(log) : [log] }
   }, {})
 
-  const groupArrays = groups && Object.keys(groups).map((date) => {
+  const groupArrays = Object.keys(groups).map((date) => {
     return {
       date,
       activities: groups[date]
     }
   })
-
+  
   return (
     <>
       <div className="mt-4 max-h-full overflow-y-auto ">
-        {groupArrays
+        {groupArrays.length > 0
           ? groupArrays.map((data: { date: string, activities: ActivityEventMessage[] | undefined }) => {
             return (
-              <>
+              <div key={data.date}>
                 <div className='bg-red-300'><DateSeparator date={data.date}/></div>
                 {data.activities?.map((element, index) => {
                   return <LogElement key={element.time} time={element.time} msg={element.msg} type={element.type} />
                 })}
-              </>
+              </div>
             )
           })
           : <p>No activity to show</p>
