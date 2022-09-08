3044b407a521feb432aeec57222e2c7b0fa9a693
Pedro Oliveira <poliveira@subvisual.co>
2022-09-08 10:12:16 +0100

    feat: ipc interface for event listners

--- renderer/src/components/DashboardStats.tsx
+++ renderer/src/components/DashboardStats.tsx
@@ -6,22 +6,20 @@ type IStationStats = {
 }
 
 const StationStats: FC<IStationStats> = ({ totalJobs, totalEarns }) => {
-
   return (
       <>
         <div className='basis-1/2 flex flex-col min-w-[50%]'>
           <p className='text-blue-600'>Total Jobs Completed</p>
-          <p className='text-3xl text-blue-600'>{totalJobs ? totalJobs : '---'}</p>
+          <p className='text-3xl text-blue-600'>{totalJobs || '---'}</p>
         </div>
         <div className='basis-1/2 flex flex-col min-w-[50%]'>
           <p className='text-blue-600'>Total Earnings</p>
           <p className='text-3xl text-blue-600'>
-            {totalEarns ? totalEarns : '---'}
+            {totalEarns || '---'}
             {totalEarns && <span className='ml-1 text-base'>FIL</span>}
           </p>
         </div>
       </>
-
   )
 }
 
