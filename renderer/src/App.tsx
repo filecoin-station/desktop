d5838f5e87f61fb235931ebb03c5156bf9e112ee
Pedro Oliveira <poliveira@subvisual.co>
2022-09-20 09:45:50 +0100

    feat: add style to login page

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,23 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import AppRoutes from './configs/Routes'
 import './App.css'
-import { useEffect } from 'react'
-import Onboarding from './pages/Onboarding'
-import WalletConfig from './pages/WalletConfig'
+import { ActivityLog } from './components/ActivityLog'
+import { TotalJobsCompleted } from './components/TotalJobsCompleted'
 import Saturn from './Saturn'
-// import { ActivityLog } from './components/ActivityLog'
-// import { TotalJobsCompleted } from './components/TotalJobsCompleted'
 
 const App = ():JSX.Element => {
-  useEffect(() => { document.title = 'Filecoin Station' })
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Onboarding />} />
-        <Route path="/wallet" element={<WalletConfig />} />
-        <Route path="/dashboard" element={<Saturn />} />
-
-      </Routes>
-    </Router>
+    <AppRoutes />
   )
 }
 
