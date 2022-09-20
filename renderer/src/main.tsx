266b5ffa4ab8bf225169abe74284e14d37674b60
Miroslav Bajtoš <saturn@bajtos.net>
2022-09-20 14:46:21 +0200

    feat: setup Sentry error monitoring
    Signed-off-by: Miroslav Bajtoš <saturn@bajtos.net>

--- renderer/src/main.tsx
+++ renderer/src/main.tsx
@@ -1,8 +1,20 @@
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
+
+Sentry.init({
+  dsn: 'https://ff9615d8516545158e186d863a06a0f1@o1408530.ingest.sentry.io/6762462',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+})
 
 ReactDOM.createRoot(
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
