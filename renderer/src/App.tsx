46d026c9adab0a7d1fd862648d5385063c6a0982
Pedro Oliveira <poliveira@subvisual.co>
2022-09-20 11:25:00 +0100

    test: address input unit testing

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,10 +1,13 @@
-import AppRoutes from './configs/Routes'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { ActivityLog } from './components/ActivityLog'
-import { TotalJobsCompleted } from './components/TotalJobsCompleted'
+import { useEffect } from 'react'
+import WalletConfig from './pages/WalletConfig'
 import Saturn from './Saturn'
+// import { ActivityLog } from './components/ActivityLog'
+// import { TotalJobsCompleted } from './components/TotalJobsCompleted'
 
 const App = ():JSX.Element => {
+  useEffect(() => { document.title = 'Filecoin Station' })
   return (
     <Router>
       <Routes>
