c7c630a4c666aa1768b25d8052918a1cdc598a46
Julian Gruber <julian@juliangruber.com>
2022-09-22 15:25:37 +0200

    add telemetry (wip)

--- main/job-stats.js
+++ main/job-stats.js
@@ -1,6 +1,22 @@
 'use strict'
 
+const { InfluxDB, Point } = require('@influxdata/influxdb-client')
 const Store = require('electron-store')
+
+const client = new InfluxDB({
+  url: 'https://eu-central-1-1.aws.cloud2.influxdata.com',
+  // station-anonymous-write
+  token: 'P9INe7-DkMB_oYSAXgw0IlBcqRc4bDi05jZFz0NZd8aApv5lieqBiN59M8cmHs6yNMRtly8_KD9RYxnyrnx8dA=='
+})
+const writeClient = client.getWriteApi(
+  'julian.gruber@protocol.ai',
+  'station',
+  'ns'
+)
+
+setInterval(() => {
+  writeClient.flush()
+}, 1000)
 
 /** @typedef {import('./typings').ModuleJobStatsMap} ModuleJobStatsMap */
 
@@ -13,6 +29,7 @@ class JobStats {
 
   constructor () {
     this.#perModuleJobStats = loadStoredStats()
+    this.#updateStats()
   }
 
   getTotalJobsCompleted () {
@@ -26,6 +43,7 @@ class JobStats {
   setModuleJobsCompleted (moduleName, count) {
     this.#perModuleJobStats[moduleName] = count
     this.#save()
+    this.#updateStats()
   }
 
   reset () {
@@ -35,6 +53,16 @@ class JobStats {
 
   #save () {
     storeStats(this.#perModuleJobStats)
+  }
+
+  #updateStats () {
+    const point = new Point('jobs-completed')
+    for (const [moduleName, count] of Object.entries(this.#perModuleJobStats)) {
+      point
+        .tag('module', moduleName)
+        .intField('value', count)
+    }
+    writeClient.writePoint(point)
   }
 }
 
