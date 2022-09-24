af7714f3ede50f410df053b850aa5b684b6626e9
Pedro Oliveira <poliveira@subvisual.co>
2022-09-24 22:27:26 +0100

    sync with login

--- renderer/src/pages/Onboarding.tsx
+++ renderer/src/pages/Onboarding.tsx
@@ -21,6 +21,8 @@ const OnboardingPage = (): JSX.Element => {
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [isOnboardingCompleted, setIsOnboardingCompleted] = useState<boolean|null>()
 
+  useEffect(() => { document.title = 'Filecoin Station' })
+
   useEffect(() => {
     (async () => {
       await sleep(2000)
