60bebb370cf8c83628687408bc4796f33ca92735
Pedro Oliveira <poliveira@subvisual.co>
2022-09-24 22:12:46 +0100

    merge with login

--- renderer/src/lib/station-config.tsx
+++ renderer/src/lib/station-config.tsx
@@ -1,5 +1,13 @@
 import { ActivityEventMessage } from '../typings'
 
+export async function getOnboardingCompleted (): Promise<boolean> {
+  return await window.electron.stationConfig.getOnboardingCompleted()
+}
+
+export async function setOnboardingCompleted (): Promise<void> {
+  return await window.electron.stationConfig.setOnboardingCompleted()
+}
+
 export async function getFilAddress (): Promise<string | undefined> {
   return await window.electron.stationConfig.getFilAddress()
 }
