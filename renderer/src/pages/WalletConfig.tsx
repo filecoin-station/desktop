8dddd3550dcc41a55bd59ef57186783610a3d6bd
Pedro Oliveira <jopedroliveira@users.noreply.github.com>
2022-09-28 12:14:45 +0100

    refactor: remove address check from walletconf (#171)
    * refactor: rmv stored address check from walletconf
    
    * refactor: move saturn start call to interface
    
    * refactor: add startSaturnNode to test mock
    
    Co-authored-by: Julian Gruber <julian@juliangruber.com>

--- renderer/src/pages/WalletConfig.tsx
+++ renderer/src/pages/WalletConfig.tsx
@@ -1,31 +1,17 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useCallback } from 'react'
 import FilAddressForm from '../components/FilAddressForm'
 import BackgroundGraph from './../assets/img/graph.svg'
 import { useNavigate } from 'react-router-dom'
-import { getFilAddress, setFilAddress as saveFilAddress } from '../lib/station-config'
+import { startSaturnNode, setFilAddress as saveFilAddress } from '../lib/station-config'
 
 const WalletConfig = (): JSX.Element => {
   const navigate = useNavigate()
 
-  const [filAddress, setFilAddress] = useState<string | undefined>(undefined)
-
-  useEffect(() => {
-    (async () => {
-      setFilAddress(await getFilAddress())
-    })()
-  }, [])
-
   const setStationFilAddress = useCallback(async (address: string | undefined) => {
     await saveFilAddress(address)
-    window.electron.saturnNode.start()
-    setFilAddress(address)
-  }, [])
-
-  useEffect(() => {
-    if (filAddress && filAddress !== '') {
-      navigate('/dashboard', { replace: true })
-    }
-  }, [filAddress, navigate])
+    startSaturnNode()
+    navigate('/dashboard', { replace: true })
+  }, [navigate])
 
   return (
     <div className="w-full h-full relative">
