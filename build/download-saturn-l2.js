de236e0dce49008417209746303e0f90c17c8f9d
Julian Gruber <julian@juliangruber.com>
2022-10-31 16:07:24 +0100

    Fix saturn-L2-node permission
    The build process requires the binary to be readable

--- build/download-saturn-l2.js
+++ build/download-saturn-l2.js
@@ -76,7 +76,7 @@ async function main () {
                 if (entry.path === 'L2-node') {
                   const outPath = `${outDir}/l2node-darwin-x64/saturn-L2-node`
                   await pipeline(entry, createWriteStream(outPath))
-                  await chmod(outPath, 0o111)
+                  await chmod(outPath, 0o555)
                   return
                 }
               }
