034e18ffbf401148a787b69de44d570f67fd13e0
Pedro Oliveira <poliveira@subvisual.co>
2022-12-16 13:54:28 +0000

    refactor: staturn fe component startup

--- renderer/src/components/Saturn.tsx
+++ renderer/src/components/Saturn.tsx
@@ -1,13 +1,9 @@
 import { useEffect } from 'react'
-import { getDestinationWalletAddress, startSaturnNode } from '../lib/station-config'
+import { startSaturnNode } from '../lib/station-config'
 
 const Saturn = () => {
   useEffect(() => {
-    (async () => {
-      if (await getDestinationWalletAddress()) {
-        startSaturnNode()
-      }
-    })()
+    startSaturnNode()
   }, [])
 
   return null
