017433e257aa897fe2edf0c48af982fb96172784
Julian Gruber <julian@juliangruber.com>
2023-01-05 13:43:37 +0100

    disable transfer FIL with balance 0

--- renderer/src/components/TransferFunds.tsx
+++ renderer/src/components/TransferFunds.tsx
@@ -16,7 +16,7 @@ const TransferFundsButtons: FC<TransferFundsButtonsProps> = ({ transferMode, bal
 
   useEffect(() => { setInternalTransferMode(transferMode) }, [transferMode])
 
-  const disabled = !destinationFilAddress
+  const disabled = !destinationFilAddress || balance === 0
   return (
     <div className={`relative flex items-end w-full ease-[cubic-bezier(0.85,0,0.15,1)] duration-700 ${(!editMode) ? 'visible' : 'z-0 -translate-y-[7.8rem] text-opacity-0 opacity-0'} ${(editMode && disabled) && 'invisible opacity-0'}`}>
       <div className={`absolute w-fit right-0 flex items-center ease-[cubic-bezier(0.85,0,0.15,1) duration-500 z-10 ${(internalTransferMode && !disabled) ? '-translate-x-[8rem] opacity-0' : ''}`}>
@@ -26,9 +26,9 @@ const TransferFundsButtons: FC<TransferFundsButtonsProps> = ({ transferMode, bal
           <span className="text-2xs px-4 text-body-s text-primary">Transfer FIL</span>
         </button>
         {disabled &&
-          <div className="absolute -left-[13px] mb-[1px] hover:-left-[89px] hover:-top-[69.5px] flex flex-col items-center group">
+          <div className="absolute -left-[13px] mb-[1px] hover:-left-[89px] hover:-top-[87.5px] flex flex-col items-center group">
             <div className='w-44 px-2 py-4 mb-[13px] rounded-lg bg-grayscale-200 hidden group-hover:block'>
-              <p className='text-body-2xs text-center'>We need a FIL address to transfer your FIL</p>
+              <p className='text-body-2xs text-center'>We need a FIL address and positive balance to transfer your FIL</p>
             </div>
             <InfoIcon className="fill-grayscale-400" width={'24px'} height={'24px'} />
           </div>
