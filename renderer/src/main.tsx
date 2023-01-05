d28b574d82c17789de3688e003561d7b213f3bb9
Miroslav Bajtoš <saturn@bajtos.net>
2023-01-05 15:01:24 +0100

    fix buffer problem by downgrading address module (#379)
    * fix buffer problem by downgrading address module
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>
    
    * fixup! remove rollup-inject
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- renderer/src/main.tsx
+++ renderer/src/main.tsx
@@ -2,10 +2,6 @@ import './index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Buffer from 'buffer/'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-window.Buffer = Buffer as any
 
 ReactDOM.createRoot(
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
