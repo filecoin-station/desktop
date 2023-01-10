51bf5d98f989ba07a504568e850c4a0a7b6a2eb9
Julian Gruber <julian@juliangruber.com>
2023-01-10 15:03:05 +0100

    fix button text overflow

--- renderer/src/components/TransferFunds.tsx
+++ renderer/src/components/TransferFunds.tsx
@@ -21,7 +21,7 @@ const TransferFundsButtons: FC<TransferFundsButtonsProps> = ({ transferMode, bal
   return (
     <div className={`relative flex items-end w-full ease-[cubic-bezier(0.85,0,0.15,1)] duration-700 ${(!editMode) ? 'visible' : 'z-0 -translate-y-[7.8rem] text-opacity-0 opacity-0'} ${(editMode && disabled) && 'invisible opacity-0'}`}>
       <div className={`absolute w-fit right-0 flex items-center ease-[cubic-bezier(0.85,0,0.15,1) duration-500 z-10 ${(internalTransferMode && !disabled) ? '-translate-x-[8rem] opacity-0' : ''}`}>
-        <button className="btn-primary w-40 bg-grayscale-250 text-primary"
+        <button className="btn-primary w-48 bg-grayscale-250 text-primary"
           disabled={disabled}
           onClick={enableTransferMode}>
           <span className="text-2xs px-4 text-body-s text-primary">Transfer FIL</span>
@@ -36,7 +36,7 @@ const TransferFundsButtons: FC<TransferFundsButtonsProps> = ({ transferMode, bal
         }
       </div>
       <div className={`absolute w-fit right-0 flex gap-1 items-center ${internalTransferMode ? 'z-20' : 'z-0'} `}>
-        <button className={`btn-primary w-40 bg-grayscale-250 text-primary ease-[cubic-bezier(0.85,0,0.15,1) duration-500 ${internalTransferMode ? '' : 'translate-x-[7.6rem] text-opacity-0 '} `}
+        <button className={`btn-primary w-48 bg-grayscale-250 text-primary ease-[cubic-bezier(0.85,0,0.15,1) duration-500 ${internalTransferMode ? '' : 'translate-x-[7.6rem] text-opacity-0 '} `}
           onClick={transferAllFunds}>
           <span className="text-2xs px-4 text-body-s">
             Send{' '}
