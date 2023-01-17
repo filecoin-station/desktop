1b5e05dcfe4f16f8d36f849233e406fe89f53fc6
Pedro Oliveira <poliveira@subvisual.co>
2023-01-17 17:51:17 +0000

    test: expect new wallet-onboarding card on e2e test

--- test/e2e/app-launch.e2e.test.js
+++ test/e2e/app-launch.e2e.test.js
@@ -61,6 +61,7 @@ test.describe.serial('Application launch', async () => {
   test('navigate to dashboard', async () => {
     await mainWindow.click('button:has-text("Continue")')
     await mainWindow.click('button:has-text("Continue")')
+    await mainWindow.click('button:has-text("Continue")')
     await mainWindow.click('button:has-text("Accept")')
     expect(new URL(await mainWindow.url()).pathname).toBe('/dashboard')
   })
