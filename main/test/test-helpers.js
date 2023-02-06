f162530568f15c5576eac44bc630ae91b7f0a49a
Julian Gruber <julian@juliangruber.com>
2023-02-06 13:43:12 +0100

    relax test helper

--- main/test/test-helpers.js
+++ main/test/test-helpers.js
@@ -26,7 +26,7 @@ function pickProps (data, ...propNames) {
  * @param {number} actualValue
  * @param {number} maxDeltaInMs
  */
-function assertTimestampIsCloseToNow (actualValue, valueDescription = 'timestamp', maxDeltaInMs = 500) {
+function assertTimestampIsCloseToNow (actualValue, valueDescription = 'timestamp', maxDeltaInMs = 1000) {
   const now = Date.now()
   const delta = Math.abs(actualValue - now)
 
