8719f0c1648f3f123e28717f0bc52c580879c458
Julian Gruber <julian@juliangruber.com>
2023-02-06 23:40:12 +0100

    force disable notarize

--- build/notarize-macos.js
+++ build/notarize-macos.js
@@ -1,31 +1,31 @@
 'use strict'
 
-const { notarize } = require('@electron/notarize')
+// const { notarize } = require('@electron/notarize')
 
-const isSet = (/** @type {string | undefined} */ value) => value && value !== 'false'
+// const isSet = (/** @type {string | undefined} */ value) => value && value !== 'false'
 
 /**
  * electron-build hook responsible for Apple Notarization of signed DMG
  *
- * @param {import('electron-builder').AfterPackContext} context
+ * // @param {import('electron-builder').AfterPackContext} context
  */
-exports.default = async function notarizing (context) {
-  const { electronPlatformName, appOutDir } = context
-  if (electronPlatformName !== 'darwin') return
-  // skip notarization if secrets are not present in env
-  if (!process.env.APPLEID || !process.env.APPLEIDPASS) return
-  // skip notarization when signing is disabled in PRs
-  // https://github.com/electron-userland/electron-builder/commit/e1dda14
-  if (isSet(process.env.TRAVIS_PULL_REQUEST) ||
-    isSet(process.env.CI_PULL_REQUEST) ||
-    isSet(process.env.CI_PULL_REQUESTS)) return
+exports.default = async function notarizing (/* context */) {
+  // const { electronPlatformName, appOutDir } = context
+  // if (electronPlatformName !== 'darwin') return
+  // // skip notarization if secrets are not present in env
+  // if (!process.env.APPLEID || !process.env.APPLEIDPASS) return
+  // // skip notarization when signing is disabled in PRs
+  // // https://github.com/electron-userland/electron-builder/commit/e1dda14
+  // if (isSet(process.env.TRAVIS_PULL_REQUEST) ||
+  //   isSet(process.env.CI_PULL_REQUEST) ||
+  //   isSet(process.env.CI_PULL_REQUESTS)) return
 
-  const appName = context.packager.appInfo.productFilename
+  // const appName = context.packager.appInfo.productFilename
 
-  return notarize({
-    appBundleId: 'io.filecoin.station',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS
-  })
+  // return notarize({
+  //   appBundleId: 'io.filecoin.station',
+  //   appPath: `${appOutDir}/${appName}.app`,
+  //   appleId: process.env.APPLEID,
+  //   appleIdPassword: process.env.APPLEIDPASS
+  // })
 }
