2021331484c720797cc4e3f8cfba00208b76aa5d
Julian Gruber <julian@juliangruber.com>
2023-02-22 08:08:10 -0800

    set renderer max line length to 200 (#522)
    * set renderer max line length to 200
    
    * refactor multiline strings

--- renderer/src/components/WalletOnboarding.tsx
+++ renderer/src/components/WalletOnboarding.tsx
@@ -11,7 +11,9 @@ const WalletOnboarding = () => {
             </div>
             <div className='py-3 px-6 pr-24'>
               <p className='text-body-s text-primary font-body uppercase'>YOUR STATION WALLET</p>
-              <p className='text-body-2xs mt-1'>Your Station Wallet has a unique address, where all the FIL you earn will be stored. Station Modules will send FIL to this wallet on a regular basis, based on your contributions.</p>
+              <p className='text-body-2xs mt-1'>
+                Your Station Wallet has a unique address, where all the FIL you earn will be stored. Station Modules will send FIL to this wallet on a regular basis, based on your contributions.
+              </p>
             </div>
           </div>
           <div className='flex flex-row align-middle h-[110px] w-full bg-grayscale-100 rounded'>
@@ -20,7 +22,10 @@ const WalletOnboarding = () => {
             </div>
             <div className='py-3 px-6 pr-24'>
               <p className='text-body-s text-primary font-body uppercase'>TRANSFERRING YOUR FIL</p>
-              <p className='text-body-2xs mt-1'>In order to transfer FIL out of your Station Wallet, you need to set a destination FIL address to which to send your FIL. We recommend you regularly transfer the FIL in your Station Wallet to a more secure wallet.</p>
+              <p className='text-body-2xs mt-1'>
+                In order to transfer FIL out of your Station Wallet, you need to set a destination FIL address to which to send your FIL. We recommend you regularly transfer the FIL in your Station
+                Wallet to a more secure wallet.
+              </p>
             </div>
           </div>
           <div className='flex flex-row align-middle h-[110px] w-full bg-grayscale-100 rounded'>
@@ -29,7 +34,9 @@ const WalletOnboarding = () => {
             </div>
             <div className='py-3 px-6 pr-24'>
               <p className='text-body-s text-primary font-body uppercase'>GAS FEES</p>
-              <p className='text-body-2xs mt-1'>All transactions on the blockchain incur a small gas fee, which can vary depending on network activity. This is deducted from the total amount you are transferring.</p>
+              <p className='text-body-2xs mt-1'>
+                All transactions on the blockchain incur a small gas fee, which can vary depending on network activity. This is deducted from the total amount you are transferring.
+              </p>
             </div>
           </div>
         </div>
