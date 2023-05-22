c7ae7ae229360891f1c9f5b70df9d45b993561e6
Julian Gruber <julian@juliangruber.com>
2023-05-22 11:35:46 +0200

    core: Add pass `RUNNING_IN_STATION_DESKTOP`

--- main/core.js
+++ main/core.js
@@ -114,7 +114,8 @@ async function start (core) {
       ...process.env,
       FIL_WALLET_ADDRESS: wallet.getAddress(),
       CACHE_ROOT: consts.CACHE_ROOT,
-      STATE_ROOT: consts.STATE_ROOT
+      STATE_ROOT: consts.STATE_ROOT,
+      RUNNING_IN_STATION_DESKTOP: 'true'
     }
   })
 
