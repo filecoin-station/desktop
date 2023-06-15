0a612d9ede26a681692b6cf5cf9a63fb9ebf652f
Julian Gruber <julian@juliangruber.com>
2023-06-15 13:17:15 +0200

    Add universal builds on macOS (#767)
    * Create `universal` builds on macOS
    
    * try building separately
    
    * use one build step per arch
    
    * add arch to file name
    
    * Revert "use one build step per arch"
    
    This reverts commit 4d4b398de093714316616c7ba71781aa93cb3a9c.
    
    * wip
    
    * bump `@filecoin-station/core`
    
    * use after-pack hook to rebuild core
    
    * clean up
    
    * `universal` seems to work!
    
    * undo file name change
    
    * clean up
    
    * update `electron-builder`
    
    * set `x64ArchFiles` (bacalhau arm64 is actually x64)
    
    * fix bacalhau path
    
    * Revert "fix bacalhau path"
    
    This reverts commit f0541879ffcc4c5a33e0226ef61704aa39a2d429.
    
    * Revert "set `x64ArchFiles` (bacalhau arm64 is actually x64)"
    
    This reverts commit 4d974e82a74254361966c4746b11f95f2d2685cb.
    
    * disable `USE_HARD_LINKS`

---
+++ build/after-pack.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const { spawn } = require('node:child_process')
+const { platform } = require('node:os')
+const assert = require('node:assert')
+const { join } = require('node:path')
+const { once } = require('node:events')
+
+/** @typedef {import('app-builder-lib').AfterPackContext} AfterPackContext */
+
+/**
+ * @param {AfterPackContext} context
+ */
+exports.default = async function (context) {
+  if (platform() !== 'darwin') return
+  const arch = context.arch === 1
+    ? 'x64'
+    : context.arch === 3
+      ? 'arm64'
+      : context.arch === 4
+        ? 'universal'
+        : null
+  if (arch === 'universal') return
+  assert(arch, `Unknown architecture id: ${context.arch}`)
+  console.log(`Rebuild Station Core for arch=${arch}`)
+  const ps = spawn(
+    'node',
+    ['scripts/post-install.js'],
+    {
+      cwd: join(
+        context.appOutDir,
+        'Filecoin Station.app',
+        'Contents',
+        'Resources',
+        'core'
+      ),
+      env: {
+        ...process.env,
+        TARGET_ARCH: arch
+      }
+    }
+  )
+  ps.stdout.pipe(process.stdout)
+  ps.stderr.pipe(process.stderr)
+  await once(ps, 'exit')
+}
