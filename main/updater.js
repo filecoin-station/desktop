e683df258c925911b4a10f6ef3871337aefe1a93
Miroslav Bajtoš <oss@bajtos.net>
2023-08-28 18:04:53 +0200

    feat: report auto-updater errors to Sentry (#932)
    At the moment, we are not notified about any auto-updater errors
    encountered by our users. These errors are logged and that's it.
    
    This change adds Sentry integration to report these errors to Sentry.
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- main/updater.js
+++ main/updater.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const Sentry = require('@sentry/node')
 const { BrowserWindow, Notification, app, shell } = require('electron')
 const { autoUpdater } = require('electron-updater')
 const { ipcMain } = require('electron/main')
@@ -107,6 +108,7 @@ function checkForUpdatesInBackground () {
  */
 function onUpdaterError (err) {
   log.error('error', err)
+  Sentry.captureException(err)
 
   if (!checkingManually) { return }
   checkingManually = false
