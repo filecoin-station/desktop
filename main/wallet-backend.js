c4936e0788dc31a1d3733815a5a3a08edbeff540
Julian Gruber <julian@juliangruber.com>
2023-10-18 15:41:50 +0200

    add passing test

--- main/wallet-backend.js
+++ main/wallet-backend.js
@@ -38,8 +38,17 @@ class WalletBackend {
     this.onTransactionUpdate = onTransactionUpdate
   }
 
-  async setup () {
-    const { seed, isNew } = await this.getSeedPhrase()
+  /**
+   * @param {string} [testSeed]
+   */
+  async setup (testSeed) {
+    let seed, isNew
+    if (testSeed) {
+      seed = testSeed
+      isNew = false
+    } else {
+      ({ seed, isNew } = await this.getSeedPhrase())
+    }
     this.provider = new Filecoin(new HDWalletProvider(seed), {
       apiAddress: 'https://api.node.glif.io/rpc/v0'
     })
