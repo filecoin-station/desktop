674626b98e806104f1f02e9986a789eaac1d4471
Julian Gruber <julian@juliangruber.com>
2023-11-07 19:19:16 +0100

    revert temporary change

--- renderer/src/pages/Onboarding.tsx
+++ renderer/src/pages/Onboarding.tsx
@@ -19,12 +19,12 @@ const Loading = () => {
 const OnboardingPage = (): JSX.Element => {
   const navigate = useNavigate()
   const [isLoading, setIsLoading] = useState<boolean>(true)
-  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState<boolean|null>(false)
+  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState<boolean|null>()
 
   useEffect(() => {
     (async () => {
       await sleep(2000)
-      // setIsOnboardingCompleted(await getOnboardingCompleted())
+      setIsOnboardingCompleted(await getOnboardingCompleted())
       setIsLoading(false)
     })()
   }, [navigate])
