ad3ca6091e7466b9a862f8d39767846dbbed86b7
Miroslav Bajtoš <oss@bajtos.net>
2023-11-09 09:28:29 +0100

    test: change wallet for testing scheduled rewards
    The seed used for testing `fetchAllTransactions` is used in other places
    too and as a result, the address has some rewards accrued in the
    contract.
    
    That breaks the test for `fetchScheduledRewards`.
    
    This commit fixes the problem by using a different seed for this test.
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- main/test/wallet-backend.test.js
+++ main/test/wallet-backend.test.js
@@ -3,9 +3,6 @@
 const { WalletBackend } = require('../wallet-backend')
 const assert = require('assert').strict
 
-// eslint-disable-next-line max-len
-const SEED_PHRASE_FOR_TESTS = 'insane believe defy best among myself mistake account paddle episode life music fame impact below define habit rotate clay innocent history depart slice series'
-
 describe('Wallet Backend', function () {
   const backend = new WalletBackend({ disableKeytar: true })
   /** @type {import('p-retry').default} */
@@ -42,7 +39,11 @@ describe('Wallet Backend', function () {
     it('fetches all transactions', /** @this {Mocha.Test} */ async function () {
       this.timeout(20_000)
 
-      await backend.setup(SEED_PHRASE_FOR_TESTS)
+      await backend.setup(
+        // Here we want a seed for a wallet that already has some transactions
+        // eslint-disable-next-line max-len
+        'insane believe defy best among myself mistake account paddle episode life music fame impact below define habit rotate clay innocent history depart slice series'
+      )
       await pRetry(() => backend.fetchAllTransactions(), { retries: 10 })
       assert.notStrictEqual(backend.transactions.length, 0, 'has transactions')
       for (const tx of backend.transactions) {
@@ -57,7 +58,11 @@ describe('Wallet Backend', function () {
 
   describe('fetchScheduledRewards()', function () {
     it('fetches rewards scheduled for disbursement', async function () {
-      await backend.setup(SEED_PHRASE_FOR_TESTS)
+      await backend.setup(
+        // Here we want a random seed that doesn't have any scheduled rewards
+        // eslint-disable-next-line max-len
+        'expect trouble oyster fire leave frown strong mechanic cotton harsh black since bargain paddle stereo fresh neutral math iron coral trophy slab place marine'
+      )
       const amount = await pRetry(
         () => backend.fetchScheduledRewards(),
         { retries: 10 }
