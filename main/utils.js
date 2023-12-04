b079bc5c14da808fb8d29cbb6a1396a1db5acab3
Julian Gruber <julian@juliangruber.com>
2023-12-04 17:56:41 +0100

    fix module error

--- main/utils.js
+++ main/utils.js
@@ -4,7 +4,7 @@
  * @param { number | string | undefined } amount
  */
 
-export function roundToSixDecimalPlaces (amount) {
+exports.roundToSixDecimalPlaces = (amount) => {
   if (!amount) return 0
   const roundedNumber = Number(amount).toFixed(6) // Round to 6 decimal places
   return roundedNumber.toString()
