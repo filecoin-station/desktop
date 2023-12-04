b36f4a34989e0e5b72a29a74dbd1096c3c5228a2
Julian Gruber <julian@juliangruber.com>
2023-12-04 18:00:40 +0100

    fix return type

--- main/utils.js
+++ main/utils.js
@@ -2,10 +2,11 @@
 
 /**
  * @param { number | string | undefined } amount
+ * @returns { string }
  */
 
 exports.roundToSixDecimalPlaces = (amount) => {
-  if (!amount) return 0
+  if (!amount) return '0'
   const roundedNumber = Number(amount).toFixed(6) // Round to 6 decimal places
   return roundedNumber.toString()
 }
