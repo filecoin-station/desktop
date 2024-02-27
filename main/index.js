14097b0e91583886ba7b639ca84fa3f67d68b9a0
Julian Gruber <julian@juliangruber.com>
2024-02-27 11:57:23 +0100

    Migrate from glif graphql & filfox to Beryx

--- main/index.js
+++ main/index.js
@@ -132,7 +132,7 @@ const ctx = {
   restartToUpdate: () => { throw new Error('never get here') },
   openReleaseNotes: () => { throw new Error('never get here') },
   getUpdaterStatus: () => { throw new Error('never get here') },
-  browseTransactionTracker: (/** @type {string} */ transactionHash) => { shell.openExternal(`https://filfox.info/en/message/${transactionHash}`) },
+  browseTransactionTracker: (/** @type {string} */ transactionHash) => { shell.openExternal(`https://beryx.zondax.ch/v1/search/fil/mainnet/address/${transactionHash}`) },
   showTermsOfService: () => { shell.openExternal('https://pl-strflt.notion.site/Station-Terms-Conditions-e97da76bb89f49e280c2897aebe4c41f?pvs=4') },
   transactionUpdate: (transactions) => {
     ipcMain.emit(ipcMainEvents.TRANSACTION_UPDATE, transactions)
--- main/typings.ts
+++ main/typings.ts
@@ -2,18 +2,28 @@ import { Activity, FILTransaction, FILTransactionProcessing } from '../shared/ty
 export type { FILTransactionStatus } from '../shared/typings'
 export type { Activity, FILTransaction, FILTransactionProcessing }
 
-export interface FoxMessage {
-  cid: string;
-  to: string;
-  from: string;
-  nonce: number;
+export interface BeryxTransaction {
   height: number;
-  method: string;
-  value: string;
-  timestamp: number;
-  receipt: {
-    exitCode: number;
-  };
+  canonical: boolean;
+  tx_timestamp: string;
+  tipset_cid: string;
+  block_cid: string;
+  level: number;
+  gas_used: string;
+  tx_cid: string;
+  id: string;
+  parent_id: string;
+  search_id: string;
+  tx_from: string;
+  tx_to: string;
+  amount: number;
+  status: 'Ok' | 'Fail';
+  tx_type: string;
+}
+
+export interface BeryxTransactionsResponse {
+  transactions: BeryxTransaction[];
+  next_cursor: string;
 }
 
 export interface Context {
