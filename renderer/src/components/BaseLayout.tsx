81d3dec8ca7f6683f0ecb6c0b6c763c963317fc8
David Lange <lange@finiam.com>
2024-04-19 13:39:33 +0100

    move wallet widget up

--- renderer/src/components/BaseLayout.tsx
+++ renderer/src/components/BaseLayout.tsx
@@ -1,13 +1,27 @@
-import { ReactNode } from 'react'
-import Sidebar from './Sidebar'
+import { ReactNode, useState } from 'react'
+import Sidebar from 'src/components/Sidebar'
+import UpdateBanner from 'src/components/UpdateBanner'
+import Modal from 'src/components/Modal'
+import WalletWidget from 'src/components/WalletWidget'
 
 const BaseLayout = ({ children }: {children: ReactNode}) => {
+  const [walletCurtainIsOpen, setWalletCurtainIsOpen] = useState(false)
+
+  const toggleCurtain = () => setWalletCurtainIsOpen(!walletCurtainIsOpen)
+
   return (
-    <div className='flex'>
+    <div className='h-screen w-screen overflow-hidden bg-grayscale-100'>
+      <div className='w-full flex flex-wrap justify-end'>
+        <UpdateBanner />
+        <Modal isOpen={walletCurtainIsOpen} setIsOpen={toggleCurtain} />
+        <WalletWidget onClick={toggleCurtain} />
+      </div>
+      <div className='flex w-full'>
       <Sidebar />
-      <main>
+      <main className='flex-1'>
         {children}
       </main>
+      </div>
     </div>
   )
 }
--- renderer/src/pages/dashboard/Dashboard.tsx
+++ renderer/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
 import HeaderBackgroundImage from 'src/assets/img/header.png'
-import Modal from 'src/components/Modal'
 import ActivityLog from 'src/components/ActivityLog'
-import UpdateBanner from 'src/components/UpdateBanner'
-import WalletWidget from 'src/components/WalletWidget'
 import useStationActivity from 'src/hooks/StationActivity'
 import { formatTokenValue } from 'src/utils/number-ops'
 
@@ -15,68 +11,57 @@ once a month, provided you have earned more than the payout threshold.
 
 const Dashboard = (): JSX.Element => {
   const { totalJobs, scheduledRewards, activities } = useStationActivity()
-  const [walletCurtainIsOpen, setWalletCurtainIsOpen] = useState<boolean>(false)
-  const toggleCurtain = () => setWalletCurtainIsOpen(!walletCurtainIsOpen)
 
   return (
-    <div className="h-screen w-screen overflow-hidden bg-grayscale-100">
-      <UpdateBanner />
-      <div className=''>
-        <Modal isOpen={walletCurtainIsOpen} setIsOpen={toggleCurtain} />
-      </div>
-      <div className="relative">
-        <div className="max-w-[744px] mx-auto">
-          <div
-            className="absolute left-0 z-0 top-0 w-full h-[300px] bg-no-repeat bg-center"
-            style={{
-              backgroundImage: `url(${HeaderBackgroundImage})`,
-              WebkitMaskImage: 'linear-gradient(black, transparent)',
-              maskImage: 'linear-gradient(black, transparent)'
-            }}
-          >
-          </div>
-          <div className="h-[300px] flex flex-col relative z-20">
-            <div className="flex-grow flex pt-4 justify-end justify-items-end">
-              <WalletWidget onClick={toggleCurtain} />
-            </div>
-            <div className="mb-6">
-              <p className="w-fit text-body-3xs text-grayscale-700 uppercase">Total Jobs Completed</p>
-              <p className="w-fit text-header-m font-bold font-number total-jobs" title="total jobs">
-                {totalJobs.toLocaleString()}
-              </p>
-            </div>
-            <div className="mb-6">
-              <p className="w-fit text-body-3xs text-grayscale-700 uppercase">Scheduled rewards</p>
-              <p
-                className="w-fit text-header-m font-bold font-number total-earnings"
-                title={scheduledRewardsTooltip}
-              >
-                {formatTokenValue(scheduledRewards)}<span className="text-header-3xs">&nbsp;FIL</span>
-              </p>
-            </div>
-          </div>
-        </div>
+    <div className="relative">
+      <div className="max-w-[744px] mx-auto">
         <div
-          className="pointer-events-none absolute h-14 bg-grayscale-100 w-full z-20"
+          className="absolute left-0 z-0 top-0 w-full h-[300px] bg-no-repeat bg-center"
           style={{
+            backgroundImage: `url(${HeaderBackgroundImage})`,
             WebkitMaskImage: 'linear-gradient(black, transparent)',
             maskImage: 'linear-gradient(black, transparent)'
           }}
         >
         </div>
-        <div tabIndex={0} className="h-[calc(100vh_-_300px)] overflow-y-auto pt-12 relative z-10">
-          <div className="max-w-[744px] mx-auto overflow-hidden">
-            <ActivityLog activities={activities} />
+        <div className="h-[300px] flex flex-col relative z-20">
+          <div className="mb-6">
+            <p className="w-fit text-body-3xs text-grayscale-700 uppercase">Total Jobs Completed</p>
+            <p className="w-fit text-header-m font-bold font-number total-jobs" title="total jobs">
+              {totalJobs.toLocaleString()}
+            </p>
+          </div>
+          <div className="mb-6">
+            <p className="w-fit text-body-3xs text-grayscale-700 uppercase">Scheduled rewards</p>
+            <p
+              className="w-fit text-header-m font-bold font-number total-earnings"
+              title={scheduledRewardsTooltip}
+            >
+              {formatTokenValue(scheduledRewards)}<span className="text-header-3xs">&nbsp;FIL</span>
+            </p>
           </div>
         </div>
-        <div
-          className="pointer-events-none fixed h-14 bg-grayscale-100 w-full z-10 bottom-0"
-          style={{
-            WebkitMaskImage: 'linear-gradient(transparent, black)',
-            maskImage: 'linear-gradient(transparent, black)'
-          }}
-        >
+      </div>
+      <div
+        className="pointer-events-none absolute h-14 bg-grayscale-100 w-full z-20"
+        style={{
+          WebkitMaskImage: 'linear-gradient(black, transparent)',
+          maskImage: 'linear-gradient(black, transparent)'
+        }}
+      >
+      </div>
+      <div tabIndex={0} className="h-[calc(100vh_-_300px)] overflow-y-auto pt-12 relative z-10">
+        <div className="max-w-[744px] mx-auto overflow-hidden">
+          <ActivityLog activities={activities} />
         </div>
+      </div>
+      <div
+        className="pointer-events-none fixed h-14 bg-grayscale-100 w-full z-10 bottom-0"
+        style={{
+          WebkitMaskImage: 'linear-gradient(transparent, black)',
+          maskImage: 'linear-gradient(transparent, black)'
+        }}
+      >
       </div>
     </div>
   )
