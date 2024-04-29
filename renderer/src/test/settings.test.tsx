8d52399f32e9753672678dcab81ce736dc0d3b86
David Lange <lange@finiam.com>
2024-04-29 10:51:18 +0100

    add tests

--- renderer/src/test/settings.test.tsx
+++ renderer/src/test/settings.test.tsx
@@ -1,18 +1,56 @@
-import { render } from '@testing-library/react'
-import { isOpenAtLogin } from 'src/lib/station-config'
+import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { checkForUpdates, exportSeedPhrase, isOpenAtLogin, saveModuleLogsAs } from 'src/lib/station-config'
 import Settings from 'src/pages/settings/Settings'
 import { describe, expect, test, vi } from 'vitest'
 
 vi.mock('src/lib/station-config')
 
-describe('Settings page', () => {
-  beforeAll(() => {
-    vi.mocked(isOpenAtLogin).mockReturnValue(Promise.resolve(true))
+const mocks = vi.hoisted(() => {
+  return {
+    checkForUpdates: vi.fn(),
+    exportSeedPhrase: vi.fn(),
+    saveModuleLogsAs: vi.fn()
+  }
+})
 
-    render(<Settings />)
+describe('Settings page', () => {
+  describe('Interact with start at login setting', () => {
+    test('shows openAtLogin initial state as unchecked when disabled', async () => {
+      vi.mocked(isOpenAtLogin).mockReturnValue(Promise.resolve(false))
+      render(<Settings />)
+
+      expect(await screen.findByLabelText('Start at login')).not.toBeChecked()
+    })
+
+    test('shows openAtLogin initial state as checked when enabled', async () => {
+      vi.mocked(isOpenAtLogin).mockReturnValue(Promise.resolve(true))
+      render(<Settings />)
+
+      expect(await screen.findByLabelText('Start at login')).toBeChecked()
+    })
   })
 
-  test('Shows openAtLogin initial state correctly', () => {
-    expect(document.querySelector<HTMLInputElement>('input[name="isOpenAtLogin"]')?.checked).toBe(true)
+  describe('Interact with buttons', () => {
+    beforeAll(() => {
+      vi.mocked(checkForUpdates).mockImplementation(mocks.checkForUpdates)
+      vi.mocked(exportSeedPhrase).mockImplementation(mocks.exportSeedPhrase)
+      vi.mocked(saveModuleLogsAs).mockImplementation(mocks.saveModuleLogsAs)
+
+      render(<Settings />)
+    })
+
+    test('save module logs as', async () => {
+      await waitFor(() => {
+        act(() => fireEvent.click(screen.getByText('Save module logs as...')))
+        act(() => fireEvent.click(screen.getByText('Check for updates')))
+        act(() => fireEvent.click(screen.getByText('Export seed phrase')))
+      })
+
+      await waitFor(() => {
+        expect(mocks.saveModuleLogsAs).toHaveBeenCalledOnce()
+        expect(mocks.checkForUpdates).toHaveBeenCalledOnce()
+        expect(mocks.exportSeedPhrase).toHaveBeenCalledOnce()
+      })
+    })
   })
 })
