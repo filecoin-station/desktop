80bcf043963ec4e3cf4d1d4e75d45710fca99273
David Lange <lange@finiam.com>
2024-05-10 11:55:37 +0100

    fix layout

--- renderer/src/components/Layout.tsx
+++ renderer/src/components/Layout.tsx
@@ -2,10 +2,12 @@ import { ReactNode } from 'react'
 import Sidebar from 'src/components/Sidebar'
 import UpdateBanner from 'src/components/UpdateBanner'
 import WalletWidget from 'src/components/WalletWidget'
+import DraggableArea from 'src/components/DraggableArea'
 
 const Layout = ({ children }: {children: ReactNode}) => {
   return (
-    <div className='h-screen w-screen overflow-hidden flex'>
+    <div className='h-screen w-screen overflow-x-hidden flex'>
+      <DraggableArea />
       <Sidebar />
       <main className='flex-1 px-20'>
         <div className='w-full flex flex-wrap justify-end'>
