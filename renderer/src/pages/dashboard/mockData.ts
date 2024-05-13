f3cff67cdc14e541e316928fca89a61fda4a0b4a
David Lange <lange@finiam.com>
2024-05-13 11:38:57 +0100

    add mock rewards data

---
+++ renderer/src/pages/dashboard/mockData.ts
@@ -0,0 +1,27 @@
+export const mockData = [
+  {
+    timestamp: '2024-05-09',
+    totalRewardsReceived: 0.2,
+    totalScheduledRewards: 0.2
+  },
+  {
+    timestamp: '2024-05-10',
+    totalRewardsReceived: 0.2,
+    totalScheduledRewards: 0.3
+  },
+  {
+    timestamp: '2024-05-11',
+    totalRewardsReceived: 0.2,
+    totalScheduledRewards: 0.4
+  },
+  {
+    timestamp: '2024-05-12',
+    totalRewardsReceived: 0.2,
+    totalScheduledRewards: 0.5
+  },
+  {
+    timestamp: '2024-05-13',
+    totalRewardsReceived: 0.7,
+    totalScheduledRewards: 0.7
+  }
+]
