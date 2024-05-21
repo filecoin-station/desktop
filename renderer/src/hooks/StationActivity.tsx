c7b5722e3c80ae2e4ab231a6635db98f3fc8d1b8
David Lange <lange@subvisual.co>
2024-05-21 10:53:48 +0100

    Setup dashboard chart (#1534)
    * add mock rewards data
    
    * add hook, move rewards out of activity hook
    
    * add basic chart, add deps
    
    * update tests

--- renderer/src/hooks/StationActivity.tsx
+++ renderer/src/hooks/StationActivity.tsx
@@ -1,19 +1,16 @@
 import { useState, useEffect } from 'react'
 import {
   getActivities,
-  getScheduledRewards,
   getTotalJobsCompleted
 } from 'src/lib/station-config'
 import { Activity } from 'src/typings'
 
 interface StationActivity {
   totalJobs: number;
-  scheduledRewards: string | undefined;
   activities: Activity[] | [];
 }
 const useStationActivity = (): StationActivity => {
   const [totalJobs, setTotalJobs] = useState<number>(0)
-  const [scheduledRewards, setScheduledRewards] = useState<string | undefined>()
   const [activities, setActivities] = useState<Activity[]>([])
 
   useEffect(() => {
@@ -23,11 +20,6 @@ const useStationActivity = (): StationActivity => {
 
   useEffect(() => {
     const loadStoredInfo = async () => setTotalJobs(await getTotalJobsCompleted())
-    loadStoredInfo()
-  }, [])
-
-  useEffect(() => {
-    const loadStoredInfo = async () => setScheduledRewards(await getScheduledRewards())
     loadStoredInfo()
   }, [])
 
@@ -51,16 +43,7 @@ const useStationActivity = (): StationActivity => {
     }
   }, [])
 
-  useEffect(() => {
-    const unsubscribeOnScheduledRewardsUpdate = window.electron.stationEvents.onScheduledRewardsUpdate(balance => {
-      setScheduledRewards(balance)
-    })
-    return () => {
-      unsubscribeOnScheduledRewardsUpdate()
-    }
-  }, [])
-
-  return { totalJobs, scheduledRewards, activities }
+  return { totalJobs, activities }
 }
 
 export default useStationActivity
