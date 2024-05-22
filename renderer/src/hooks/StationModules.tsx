83f6f9ec592a617565f88281a412385fce033be3
Julian Gruber <julian@juliangruber.com>
2024-05-22 14:23:18 -0700

    Add modules api (#1550)
    * add modules api
    
    * add spark logo

--- renderer/src/hooks/StationModules.tsx
+++ renderer/src/hooks/StationModules.tsx
@@ -1,24 +1,75 @@
 import { useEffect, useState } from 'react'
-import { mockData } from 'src/pages/modules/mockData'
+import * as Name from 'w3name'
+import SparkLogo from 'src/assets/img/icons/spark.png'
 
-// Using mock data
-async function getAllModules () {
-  return mockData
+/**
+ * @returns {Promise<string>} The contract address of the Spark module
+ */
+async function getSparkContractAddress () {
+  const name = Name.parse(
+    'k51qzi5uqu5dmaqrefqazad0ca8b24fb79zlacfjw2awdt5gjf2cr6jto5jyqe'
+  )
+  const revision = await Name.resolve(name)
+  const address = revision.value.split('\n').filter(Boolean).pop()
+  if (typeof address !== 'string') {
+    throw new Error('Failed to resolve Spark contract address')
+  }
+  return address
 }
 
-type Module = typeof mockData[number]
+/* eslint-disable max-len */
+const modules = {
+  spark: {
+    name: 'Spark',
+    description: 'With its array of tools, resources, and support, Spark empowers users to spark new ideas, projects, and solutions that harness the full potential of Filecoin\'s decentralized storage protocol.',
+    contractAddress: '0x',
+    logo: SparkLogo,
+    stats: { // TODO: Get this from spark-stats once available
+      totalJobs: 0,
+      totalRewards: 0
+    },
+    links: {
+      github: 'https://github.com/filecoin-station/spark',
+      docs: 'https://filspark.com/'
+    }
+  },
+  voyager: {
+    name: 'Voyager',
+    description: 'Module Y empowers users to spark new ideas, projects, and solutions that harness the full potential of Filecoin\'s decentralized storage protocol.',
+    contractAddress: '0xc524b83bf85021e674a7c9f18f5381179fabaf6c',
+    logo: 'https://assets-global.website-files.com/633bfdb71d8e552b70ceff10/633eb29c75502c0e935ff647_station-connect.svg',
+    stats: {
+      totalJobs: 0, // TODO: Updater after data export
+      totalRewards: 0 // TODO: Update after airdrop
+    },
+    links: {
+      github: 'https://github.com/filecoin-station/voyager',
+      docs: 'https://filstation.app/'
+    }
+  }
+}
 
 export const useModules = () => {
-  const [modules, setModules] = useState<Module[]>([])
+  const [sparkStats] = useState<typeof modules.spark.stats>(modules.spark.stats)
+  const [sparkContractAddress, setSparkContractAddress] = useState<string>()
 
   useEffect(() => {
-    async function loadStoredInfo () {
-      setModules(await getAllModules())
+    async function load () {
+      setSparkContractAddress(await getSparkContractAddress())
     }
-    loadStoredInfo()
+    load()
   }, [])
 
+  const modulesUpdated = {
+    ...modules,
+    spark: {
+      ...modules.spark,
+      contractAddress: sparkContractAddress,
+      stats: sparkStats
+    }
+  }
+
   return {
-    modules
+    modules: Object.entries(modulesUpdated).map(([id, mod]) => ({ ...mod, id }))
   }
 }
