66c56494b480d973ca5da52334027686aeb0520b
David Lange <lange@finiam.com>
2024-05-24 11:18:05 +0100

    add variant to input

--- renderer/src/components/TextInput.tsx
+++ renderer/src/components/TextInput.tsx
@@ -1,35 +1,65 @@
 import classNames from 'classnames'
-import { ComponentPropsWithRef } from 'react'
+import { ComponentPropsWithRef, forwardRef } from 'react'
 import Text from './Text'
 import WarningIcon from 'src/assets/img/icons/warning.svg?react'
 
-type TextInputProps = ComponentPropsWithRef<'input'> & {
+type TextInputProps = {
+  variant: 'primary' | 'secondary';
   error?: string | boolean;
+} & ComponentPropsWithRef<'input'>
+
+const variantClassNames = {
+  primary: `text-black placeholder:text-secondary focus:outline-none rounded-sm 
+  focus:placeholder-shown:ring-2 focus:placeholder-shown:ring-slate-400`,
+  secondary: `px-5 text-white placeholder:text-white border bg-black text-center
+  focus:ring-0 ring-0 focus:outline-1 rounded-[4px]`
 }
 
-const TextInput = ({ error, ...props }: TextInputProps) => {
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>(function ({
+  variant,
+  error,
+  ...props
+}, ref) {
+  const inputClassName = classNames(
+    variantClassNames[variant],
+    {
+      'border-dashed border-white focus:outline-slate-400': variant === 'secondary' && !error,
+      'border-solid border-red-400 focus:outline-red-400': variant === 'secondary' && error
+    },
+    'py-2 text-body-s w-full peer'
+  )
+
   const borderClassName = classNames({
     'border-slate-400 peer-hover:border-black peer-focus:border-black': !error,
     'border-red-400 peer-hover:border-red peer-focus:border-red': error
   }, 'w-full border-b')
 
+  const errorClassName = classNames({
+    'mt-2': variant === 'primary',
+    'mt-3': variant === 'secondary'
+  }, 'flex gap-2 items-start text-red-400')
+
   return (
     <div>
       <input
         type="text"
-        className={`py-2 text-body-s text-black placeholder:text-secondary rounded-sm w-full peer
-        focus:outline-none focus:placeholder-shown:ring-2 focus:placeholder-shown:ring-slate-400`}
+        className={inputClassName}
+        ref={ref}
         {...props}
       />
-      <div className={borderClassName}></div>
+      {variant === 'primary' && (
+        <div className={borderClassName}></div>
+      )}
       {error && (
-        <div className='flex gap-2 items-center mt-2 text-red-400'>
-          <WarningIcon />
+        <div className={errorClassName}>
+          <WarningIcon className='w-4 h-4 mt-[1px]' />
           <Text size='xs' color="red">{error}</Text>
         </div>
       )}
     </div>
   )
-}
+})
+
+TextInput.displayName = 'TextInput'
 
 export default TextInput
