c12ff067c8162cf059da3068a8b94fa44ee85742
David Lange <lange@finiam.com>
2024-05-28 10:18:42 +0100

    update wallet widget

--- renderer/src/components/WalletWidget.tsx
+++ renderer/src/components/WalletWidget.tsx
@@ -1,95 +1,36 @@
-import { useEffect, useState } from 'react'
 import useWallet from 'src/hooks/StationWallet'
 import WalletIcon from 'src/assets/img/icons/wallet.svg?react'
-import WalletTransactionStatusWidget from 'src/components/WalletTransactionStatusWidget'
-import { FILTransactionProcessing } from 'src/typings'
 import { useDialog } from './DialogProvider'
-import { formatFilValue, openExplorerLink, truncateString } from 'src/lib/utils'
-import { Link } from 'react-router-dom'
+import { formatFilValue } from 'src/lib/utils'
+import Text from './Text'
+import WalletModal from './WalletModal'
+import classNames from 'classnames'
 import { ROUTES } from 'src/lib/routes'
-
-const WalletModalContent = () => {
-  const {
-    stationAddress,
-    stationAddress0x,
-    walletBalance,
-    processingTransaction
-  } = useWallet()
-
-  return (
-    <div>
-      <p>Station address</p>
-      <div className='flex gap-4 mb-4'>
-        <p>{truncateString(stationAddress)}</p>
-        <p>{truncateString(stationAddress0x)}</p>
-      </div>
-      <div>
-        <p>Balance</p>
-        <div className='flex justify-between'>
-          {formatFilValue(walletBalance)} FIL
-          <Link to={ROUTES.wallet}>Transfer</Link>
-        </div>
-        {processingTransaction && (
-          <div className='flex justify-between'>
-            <p>
-              {processingTransaction.outgoing ? 'Sending' : 'Receiving'}{' '}
-              {formatFilValue(processingTransaction.amount)}{' '}FIL
-            </p>
-            {processingTransaction.hash && (
-              <button
-                type='button'
-                onClick={() => openExplorerLink(processingTransaction.hash)}
-              >
-                View on explorer
-              </button>
-            )}
-          </div>
-        )}
-      </div>
-    </div>
-  )
-}
 
 const WalletWidget = () => {
   const { openDialog } = useDialog()
-  const { walletBalance, processingTransaction } = useWallet()
-  const [displayTransition, setDisplayTransaction] = useState<FILTransactionProcessing|undefined>(undefined)
-
-  useEffect(() => {
-    if (processingTransaction !== undefined) {
-      setDisplayTransaction(processingTransaction)
-    }
-  }, [processingTransaction])
+  const { walletBalance } = useWallet()
 
   function handleClick () {
     openDialog({
-      content: <WalletModalContent />
+      content: <WalletModal />
     })
   }
 
+  const classNameForPage = (rest?: string) =>
+    classNames(rest, window.location.pathname === ROUTES.wallet ? 'text-white' : 'text-black')
+
   return (
-    <div className="flex cursor-pointer no-drag-area z-20 absolute">
-      <button type='button' onClick={handleClick} className='flex items-center wallet-widget'>
-        <WalletIcon />
-        <span className="text-right mx-3" title="wallet">
-          <span className='font-bold'>
-            {formatFilValue(walletBalance)}
-          </span>
-          {' '}
-          FIL
-        </span>
-      </button>
-      <div
-        className={`
-          transition duration-1000 ease-in-out opacity-0 ${processingTransaction ? 'opacity-100' : 'opacity-0'}
-        `}
-        onTransitionEnd={() => !processingTransaction && setDisplayTransaction(undefined)}
-      >
-        {displayTransition && (
-          <WalletTransactionStatusWidget processingTransaction={displayTransition} renderBackground={true} />
-        )}
-      </div>
-    </div>
+    <button
+      type='button'
+      onClick={handleClick}
+      className={classNameForPage(`absolute top-5 right-9 flex items-center gap-3 no-drag-area z-20 
+      focus-visible:outline-slate-400 focus:outline-slate-400 p-1`)}
+    >
+      <WalletIcon />
+      <Text font='mono' size='xs' bold className={classNameForPage()}>{formatFilValue(walletBalance)} FIL</Text>
+    </button>
+
   )
 }
 
