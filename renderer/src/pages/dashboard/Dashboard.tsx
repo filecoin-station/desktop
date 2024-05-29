5a95a57a422cdbc6f4a3a59b87690a16cd92a418
David Lange <lange@finiam.com>
2024-05-29 14:53:45 +0100

    style values info

--- renderer/src/pages/dashboard/Dashboard.tsx
+++ renderer/src/pages/dashboard/Dashboard.tsx
@@ -2,13 +2,15 @@ import BorderedBox from 'src/components/BorderedBox'
 import ActivityInfo from './ActivityInfo'
 import ChartController from './ChartController'
 import useStationRewards from 'src/hooks/StationRewards'
+import RewardsInfo from './RewardsInfo'
 
 const Dashboard = (): JSX.Element => {
-  const { historicalRewards } = useStationRewards()
+  const { historicalRewards, totalRewardsReceived, scheduledRewards } = useStationRewards()
 
   return (
     <main className='flex items-start gap-5 px-9 mt-28 flex-1'>
       <BorderedBox className='flex-1'>
+        <RewardsInfo totalRewardsReceived={totalRewardsReceived} scheduledRewards={scheduledRewards} />
         <ChartController historicalRewards={historicalRewards} />
       </BorderedBox>
       <ActivityInfo />
