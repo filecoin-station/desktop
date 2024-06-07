5a9aea81bcec804468d9e05ad9d581ac61c61a17
David Lange <lange@subvisual.co>
2024-06-07 15:01:02 +0100

    Highlight latest tx (#1601)
    * highlight latest tx
    
    * fix
    
    * add transition
    
    * rename
    
    * style fix

--- renderer/src/pages/wallet/TransactionHistory.tsx
+++ renderer/src/pages/wallet/TransactionHistory.tsx
@@ -1,29 +1,59 @@
 import dayjs from 'dayjs'
-import { formatFilValue, truncateString } from 'src/lib/utils'
+import { formatFilValue, openExplorerLink, truncateString } from 'src/lib/utils'
 import { FILTransaction } from 'shared/typings'
 import Text from 'src/components/Text'
 import SendIcon from 'src/assets/img/icons/send.svg?react'
 import ReceiveIcon from 'src/assets/img/icons/receive.svg?react'
 import classNames from 'classnames'
 import stationIllustration from 'src/assets/img/station-illustration.png'
+import LinkOut from 'src/assets/img/icons/link-out.svg?react'
+import { useEffect, useRef, useState } from 'react'
+import Transition from 'src/components/Transition'
 
 const TransactionHistory = ({
-  walletTransactions = []
+  walletTransactions
 }: {
-  walletTransactions: Array<FILTransaction>;
+  walletTransactions?: Array<FILTransaction>;
 }) => {
-  const className = classNames({
-    'pr-4': walletTransactions && walletTransactions?.length > 5
-  }, 'flex flex-col gap-4 h-[300px] overflow-y-scroll custom-scrollbar')
+  const [completeTransactions, setCompleteTransactions] = useState<
+  Array<FILTransaction & { isNew?: boolean }>
+  >()
+  const transactionsRef = useRef<string[]>()
+
+  useEffect(() => {
+    const complete = walletTransactions?.filter(tx => tx.status !== 'processing')
+
+    if (complete?.length !== transactionsRef.current?.length) {
+      setCompleteTransactions(complete?.map((tx) => ({
+        ...tx,
+        isNew: transactionsRef.current && !transactionsRef.current.includes(tx.hash) && !!completeTransactions
+      })))
+    }
+  }, [walletTransactions, completeTransactions])
+
+  transactionsRef.current = completeTransactions?.map(tx => tx.hash) || []
 
   return (
-    <div className={className}>
-      {walletTransactions?.length > 0
-        ? walletTransactions?.map(transaction => (
-          <div key={transaction.hash} className='flex gap-4'>
-            {transaction.outgoing ? <SendIcon /> : <ReceiveIcon />}
+    <Transition
+      on={!!walletTransactions}
+      className='flex flex-col h-[300px] overflow-y-scroll custom-scrollbar'
+    >
+      {completeTransactions && completeTransactions?.length > 0
+        ? completeTransactions?.map((transaction) => (
+          <button
+            type='button'
+            onClick={() => openExplorerLink(transaction.hash)}
+            key={transaction.hash}
+            className={classNames({
+              'pr-4': completeTransactions?.length > 5,
+              'animate-fradeFromBlue': transaction.isNew
+            }, 'flex gap-4 text-left px-5 py-2 group hover:bg-slate-100 focus:bg-slate-100 focus:outline-none')}
+          >
+            <div className="text-inherit">
+              {transaction.outgoing ? <SendIcon /> : <ReceiveIcon />}
+            </div>
             <div>
-              <Text size='s' as='p'>
+              <Text size='s' as='p' style={{ color: 'inherit' }}>
                 {transaction.outgoing
                   ? `Sent to ${truncateString(transaction.address)}`
                   : 'Received'}
@@ -36,8 +66,18 @@ const TransactionHistory = ({
                 {dayjs(transaction.timestamp).format('MMM D, YYYY HH:mm')}
               </Text>
             </div>
-            <Text font='mono' size='xs' className='ml-auto'>{formatFilValue(transaction.amount)} FIL</Text>
-          </div>
+            <div className='flex flex-col ml-auto justify-between'>
+              <Text
+                font='mono'
+                size='xs'
+                className='ml-auto'
+                style={{ color: 'inherit' }}
+              >
+                {formatFilValue(transaction.amount)} FIL
+              </Text>
+              <LinkOut className='ml-auto w-4 h-4 text-inherit opacity-0 group-hover:opacity-100' />
+            </div>
+          </button>
         ))
         : (
           <div className='flex flex-col items-center text-center flex-1'>
@@ -50,7 +90,7 @@ const TransactionHistory = ({
             </Text>
           </div>
         )}
-    </div>
+    </Transition>
   )
 }
 
