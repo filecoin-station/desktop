9d2e263d893f59e82f225bc024155a08dbc72006
Julian Gruber <julian@juliangruber.com>
2024-08-29 09:28:35 +0200

    remove unused `fetchScheduledRewards()`

--- main/test/wallet-backend.test.js
+++ main/test/wallet-backend.test.js
@@ -70,20 +70,4 @@ describe('Wallet Backend', function () {
       }
     })
   })
-
-  describe('fetchScheduledRewards()', function () {
-    it(
-      'fetches rewards scheduled for disbursement',
-      /** @this {Mocha.Test} */ async function () {
-        this.timeout(60_000)
-        // We need a new wallet that doesn't have any scheduled rewards
-        await backend.setup(randomSeed())
-        const amount = await pRetry(
-          () => backend.fetchScheduledRewards(),
-          { retries: 10 }
-        )
-        assert.strictEqual(amount.toBigInt(), 0n)
-      }
-    )
-  })
 })
